c091227fb1354986db68a3dad6f338e284e947c0
8gua <i@8gua.blog>
2018-01-15 21:50:07 +0800

    .

---
+++ -S/7b1580f5.js
@@ -0,0 +1,1 @@
+webpackJsonp([20],{Zguw:function(e,n,t){"use strict";e.exports=function(e){var n,o=e.h1,i=e.file,s=e.editor,c=e.box;n=o.val(),c=$(this).closest(".Pbox")[0],t.e(7).then(t.bind(null,"1iLP")).then(function(e){return e(i,n,s.getContent(),{dir:"!"}).done(function(e){c._rm(),$.box.alert("『"+$.escape(n||"无题")+"』保存成功"),e&&(e=i.slice(2,-3),GO.push("/~"+e))})})}}});
