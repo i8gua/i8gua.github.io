1ee389ca949ca698b70a6b7def07769c7e7eba32
8gua <i@8gua.blog>
2018-01-26 03:56:22 +0800

    .

--- release.sh
+++ release.sh
@@ -19,22 +19,22 @@ git archive master | tar -x -C $release
 cd $release
 
 
-# mv release/SUMMARY.md ./md/\!/SUMMARY.md
-# mv release/about.md ./md/\!/about.md
-# rm -rf .gitignore release.sh id_rsa.enc \
-#     ./-/md/\!/readme.md ./md/\!/readme.md \
-#     ./-/md/help ./md/help ./-/help \
-#     release .travis.yml ./-/S  \
-#     ./md/$
-# > ./-/init.toml
-# > ./md/SUMMARY.md
+mv release/SUMMARY.md ./md/\!/SUMMARY.md
+mv release/about.md ./md/\!/about.md
+rm -rf .gitignore release.sh id_rsa.enc \
+    ./-/md/\!/readme.md ./md/\!/readme.md \
+    ./-/md/help ./md/help ./-/help \
+    release .travis.yml ./-/S  \
+    ./md/$
+> ./-/init.toml
+> ./md/SUMMARY.md
 
-# git add .
-# cp $PREFIX/.gitignore $release
-# git add .gitignore
-# git commit -m 'release'
-# git push
-# # git push github
+git add .
+cp $PREFIX/.gitignore $release
+git add .gitignore
+git commit -m 'release'
+git push
+# git push github
 
 
 
