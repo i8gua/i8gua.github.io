62675bccc6b20fb63507224b1e3573eef66d118f
8gua <i@8gua.blog>
2018-01-28 14:18:37 +0800

    .

--- release.sh
+++ release.sh
@@ -27,7 +27,7 @@ rm -rf .gitignore release.sh id_rsa.enc \
     ./-/\!case.md ./md/\!/case.md \
     ./-/dev ./md/dev \
     release .travis.yml ./-/S  \
-    ./-/$/*.md robots.txt\
+    ./-/$/*.md robots.txt sogousiteverification.txt \
     ./-/sitemap.*
 > ./-/init.toml
 > ./md/SUMMARY.md
